3eda06d8197aaa8ff0b0213575768dd811f44ae4
Sarakvitou <Sarakvitou@gmail.com>
2019-11-08 13:53:43 -0800

    add unit test file for QueryExtensionAsync.cs

---
+++ QueryBuilder.Tests/QueryExtensionsAsyncTests.cs
@@ -0,0 +1,19 @@
+﻿using SqlKata.Tests.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace SqlKata.Tests
+{
+    public class QueryExtensionsAsyncTests : TestSupport
+    {
+
+        [Fact]
+        public void TestGetAsync()
+        {
+
+            Assert.True(true);
+        }
+    }
+}
