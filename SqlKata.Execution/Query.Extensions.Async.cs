4483e4fda791f1a40f471208d410ac2f67883dff
Sarakvitou <Sarakvitou@gmail.com>
2019-12-11 09:33:22 -0800

    Add cancel. Token to Task<dynamic> FirstOrDefaultAsync func

--- SqlKata.Execution/Query.Extensions.Async.cs
+++ SqlKata.Execution/Query.Extensions.Async.cs
@@ -22,9 +22,9 @@ namespace SqlKata.Execution
             return await QueryHelper.CreateQueryFactory(query).FirstOrDefaultAsync<T>(query, cancellationToken);
         }
 
-        public static async Task<dynamic> FirstOrDefaultAsync(this Query query)
+        public static async Task<dynamic> FirstOrDefaultAsync(this Query query, CancellationToken cancellationToken = default)
         {
-            return await FirstOrDefaultAsync<dynamic>(query);
+            return await FirstOrDefaultAsync<dynamic>(query, cancellationToken);
         }
 
         public static async Task<T> FirstAsync<T>(this Query query)
